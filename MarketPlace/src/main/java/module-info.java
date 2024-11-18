28525e125ecdbe7efd4a859d50a178bb34808fd1
Storm <167824736+St0rm20@users.noreply.github.com>
2024-11-17 23:49:00 -0500

    Add files via upload

--- MarketPlace/src/main/java/module-info.java
+++ MarketPlace/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.edu.uniquindio.co.marketplace {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-
+    requires jdk.jshell;
 
     exports com.edu.uniquindio.co.marketplace.clases;
     opens com.edu.uniquindio.co.marketplace.clases to javafx.fxml;
@@ -14,4 +14,7 @@ module com.edu.uniquindio.co.marketplace {
     opens com.edu.uniquindio.co.marketplace.clases.market to javafx.fxml;
     opens com.edu.uniquindio.co.marketplace.clases.personas to javafx.fxml;
     opens com.edu.uniquindio.co.marketplace.clases.util to javafx.fxml;
+    opens com.edu.uniquindio.co.marketplace.clases.controladores to javafx.fxml;
+    exports com.edu.uniquindio.co.marketplace.clases.interfaces;
+    opens com.edu.uniquindio.co.marketplace.clases.interfaces to javafx.fxml; // Ensure this line is present
 }
